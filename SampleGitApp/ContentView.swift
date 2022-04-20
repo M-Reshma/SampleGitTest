09f154dae20cc5285731125968bed41f164a731d
Mallikarjun <appwhizzydevelopment@gmail.com>
2022-04-20 18:49:07 +0530

    Initial Commit

---
+++ SampleGitApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleGitApp
+//
+//  Created by Mallikarjuna Reddy Punuru on 20/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleGitApp/SampleGitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleGitAppApp.swift
+//  SampleGitApp
+//
+//  Created by Mallikarjuna Reddy Punuru on 20/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleGitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

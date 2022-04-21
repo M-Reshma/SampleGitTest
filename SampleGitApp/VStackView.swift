d12c0815542b55b1435b3c2ae92bfd3851a5b9e9
M-Reshma <63777877+M-Reshma@users.noreply.github.com>
2022-04-21 12:25:32 +0530

    Vstack
    Vstack view added

---
+++ SampleGitApp/VStackView.swift
@@ -0,0 +1,23 @@
+//
+//  VStackView.swift
+//  SampleGitApp
+//
+//  Created by Mallikarjuna Reddy Punuru on 21/04/22.
+//
+
+import SwiftUI
+
+struct VStackView: View {
+    var body: some View {
+        VStack {
+        Text("Zoya")
+       Text("Armaz")
+        }
+    }
+}
+
+struct VStackView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStackView()
+    }
+}

af59c809e50f8c34292946ec5e9d7560a93934cb
M-Reshma <63777877+M-Reshma@users.noreply.github.com>
2022-04-21 11:09:02 +0530

    Hstack View
    Added Hstack View file

---
+++ SampleGitApp/HStackView.swift
@@ -0,0 +1,23 @@
+//
+//  HStackView.swift
+//  SampleGitApp
+//
+//  Created by Mallikarjuna Reddy Punuru on 21/04/22.
+//
+
+import SwiftUI
+
+struct HStackView: View {
+    var body: some View {
+        HStack {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Reshma")
+        }
+    }
+}
+
+struct HStackView_Previews: PreviewProvider {
+    static var previews: some View {
+        HStackView()
+    }
+}
